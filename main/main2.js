ae770ecec60ffd74f10b605b85a7f3d470b01cb5
KingaPestka <c00249160@itcarlow.ie>
2021-04-26 20:12:48 +0100

    updated bar
    updated bar

--- main/main2.js
+++ main/main2.js
@@ -4,11 +4,15 @@ var canvas = document.getElementById("the_canvas");
 var context = canvas.getContext("2d");
 var img = document.getElementById("backgound1");
 
-context.drawImage(img,10,10);
+ /*context.drawImage(img,10,10);/*there is an error but it works? */
  context.fillRect(500,490,200,300);
  context.fillRect(710,490,200,300);
  context.fillRect(920,490,200,300);
  context.fillRect(1130,490,200,300);
+
+
+
+
 
 }
 var correctSequence = [3,2,4,1];
@@ -21,6 +25,7 @@ function setPlayerSequence() {
 	if (howManyButtonsClicked == 1) {
 		playerSequence[0] = buttonSelection;
 		checkCorrectSequence();
+		
 	}
 	if (howManyButtonsClicked == 2) {
 		playerSequence[1] = buttonSelection;
@@ -42,17 +47,20 @@ function checkCorrectSequence() {
 		loopThroughSequence = loopThroughSequence + 1;
 		//loseHealth();
 		alert("the bar goes down by 25%");
+		health(25);
         
 	}		
 	else {
 		console.log("Sequence is not correct!");
 		alert("You sequence is not correct!");
-		
+		document.getElementById("heal").value = "100";
 		resetSequence();
 		//restoreHealth();
 	}
 	if (playerSequence[3] == correctSequence[3]) {
+		
 		murderSolvedSuccessfully();
+	
 	}
 	
 }
@@ -79,23 +87,33 @@ function resetSequence() {
 function button1() {
 	buttonSelection = 1;
 	howManyButtonsClicked = howManyButtonsClicked + 1;
-	setPlayerSequence()
+	setPlayerSequence();
+	
 }
+
 
 function button2() {
 	buttonSelection = 2;
 	howManyButtonsClicked = howManyButtonsClicked + 1;
-	setPlayerSequence()
+	setPlayerSequence();
 }
 
 function button3() {
 	buttonSelection = 3;
 	howManyButtonsClicked = howManyButtonsClicked + 1;
-	setPlayerSequence()
+	setPlayerSequence();
 }
 
 function button4() {
 	buttonSelection = 4;
 	howManyButtonsClicked = howManyButtonsClicked + 1;
-	setPlayerSequence()
+	setPlayerSequence();
+}
+
+
+function health(number) {
+var nb = parseFloat( document.getElementById("heal").value - number	);//parsefloat allows me to transform the text to a number
+console.log(nb);
+document.getElementById("heal").value = nb;
+
 }
