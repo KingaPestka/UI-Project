0b632343a78ce5f3b8a55b0d69c6ea4f07d6b617
KingaPestka <c00249160@itcarlow.ie>
2021-04-26 20:20:35 +0100

    update

--- main/main.js
+++ main/main.js
@@ -4,11 +4,15 @@ var canvas = document.getElementById("the_canvas");
 var context = canvas.getContext("2d");
 var img = document.getElementById("backgound1");
 
-context.drawImage(img,10,10);
+ /*context.drawImage(img,10,10);/*there is an error but it works? */
  context.fillRect(500,490,200,300);
  context.fillRect(710,490,200,300);
  context.fillRect(920,490,200,300);
  context.fillRect(1130,490,200,300);
+
+
+
+
 
 }
 var correctSequence = [3,2,4,1];
@@ -21,6 +25,7 @@ function setPlayerSequence() {
 	if (howManyButtonsClicked == 1) {
 		playerSequence[0] = buttonSelection;
 		checkCorrectSequence();
+		
 	}
 	if (howManyButtonsClicked == 2) {
 		playerSequence[1] = buttonSelection;
@@ -42,17 +47,20 @@ function checkCorrectSequence() {
 		loopThroughSequence = loopThroughSequence + 1;
 		//loseHealth();
 		alert("the bar goes down by 25%");
+		health(25);
         
 	}		
 	else {
 		console.log("Sequence is not correct!");
 		alert("You sequence is not correct!");
-		
+		document.getElementById("heal").value = "100";
 		resetSequence();
 		//restoreHealth();
 	}
 	if (playerSequence[3] == correctSequence[3]) {
+		
 		murderSolvedSuccessfully();
+	
 	}
 	
 }
@@ -79,88 +87,33 @@ function resetSequence() {
 function button1() {
 	buttonSelection = 1;
 	howManyButtonsClicked = howManyButtonsClicked + 1;
-	setPlayerSequence()
+	setPlayerSequence();
+	
 }
+
 
 function button2() {
 	buttonSelection = 2;
 	howManyButtonsClicked = howManyButtonsClicked + 1;
-	setPlayerSequence()
+	setPlayerSequence();
 }
 
 function button3() {
 	buttonSelection = 3;
 	howManyButtonsClicked = howManyButtonsClicked + 1;
-	setPlayerSequence()
+	setPlayerSequence();
 }
 
 function button4() {
 	buttonSelection = 4;
 	howManyButtonsClicked = howManyButtonsClicked + 1;
-	setPlayerSequence()
+	setPlayerSequence();
 }
-/*var playerSequence = new Array();
-var correctSequence = ["button3","button1","button2","button4"];
-function buttonOnClickEvent(){
-    alert("Booooommmmmm!!!");
-}
-function GamerInput(input) {
-    this.action = input;
-}
-    
 
 
-function button1()
-{
-    
-    
-    playerSequence.add(buttonOnClickEvent);
-    if(playerSequence.lenght == 4){
-        verifySequence();
-        console.log("running");
-    }
+function health(number) {
+var nb = parseFloat( document.getElementById("heal").value - number	);//parsefloat allows me to transform the text to a number
+console.log(nb);
+document.getElementById("heal").value = nb;
+
 }
-function button2()
-{
-    playerSequence.add();
-    if(playerSequence.lenght == 4){
-        verifySequence();
-        console.log("running");
-    }
-function button3()
-{
-    playerSequence.add();
-    if(playerSequence.lenght == 4){
-        verifySequence();
-        console.log("running");
-    }
-}
-function button4()
-{
-    playerSequence.add();
-    if(playerSequence.lenght == 4){
-        verifySequence();
-        console.log("running");
-    }
-}
-function solveMurder()
-{
-    playerSequence.add();
-    if(playerSequence.lenght == 4){
-        verifySequence();
-        console.log("running");
-    }
-}
-function verifySequence(){
-    if(playerSequence.lenght != correctSequence.lenght){
-        return;
-    }
-    for(var i= 0;i<4;i++){
-        if(playerSequence[i] != correctSequence[i]){
-            playerSequence.clear ();
-            return;
-        }
-    }
-    debug.Log ("yes");
-}
-}*/
